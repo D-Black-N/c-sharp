da7c8a9e25384158a8461fdf5b36265cd562a3f5
Danil Nasibullin <grandvitaro07@gmail.com>
2022-09-12 12:41:37 +0300

    done lr2 point 1

--- App/Program.cs
+++ App/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Application {
+  class MyArrays {
+    public static void Main(string[] args) {
+      int[] a = { 1, 8, 2, 10, 6 };
+      int[] b = { 7, 9, 10, 12 };
+      Array.Sort(a);
+      Array.Sort(b);
+      Array.Reverse(b);
+      foreach (var item in a) {
+        Console.Write($"{item}, ");
+      }
+      Console.WriteLine();
+      foreach (var item in b) {
+        Console.Write($"{item}, ");
+      }
+      Console.WriteLine();
+      int[] c = a.Concat(b).ToArray();
+      foreach(var item in c) {
+        Console.Write($"{item}, ");
+      }
+      Console.WriteLine();
+    }
+  }
+}
